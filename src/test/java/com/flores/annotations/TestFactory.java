ca47dffa0a97c7c9335f74730e3aa356092688ea
Jason <floresj4@gmail.com>
2015-08-18 19:05:00 -0400

    Adding unit tests for annotationed object state

--- src/test/java/com/flores/annotations/TestFactory.java
+++ src/test/java/com/flores/annotations/TestFactory.java
@@ -1,10 +1,20 @@
 package com.flores.annotations;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.flores.annotations.components.ComboComponent;
+
 public class TestFactory {
+	@BeforeClass
+	public static void init() {
+		PropertyConfigurator.configure("./src/test/resources/log4j.properties");
+	}
+
 	@Test
 	public void testSimpleConstruction() throws Exception {
 		ComponentImpl component = ComponentFactory.getComponent(ComponentType.RADIO);
@@ -12,5 +22,17 @@ public class TestFactory {
 		
 		component = ComponentFactory.getComponent(ComponentType.COMBO);
 		assertEquals(ComponentType.COMBO, component.type);
+	}
+	
+	@Test
+	public void testAnnotatedConstruction() throws Exception {
+		ComponentImpl component = ComponentFactory.getComponent(ComponentType.COMBO);
+		assertEquals(ComponentType.COMBO, component.type);
+		
+		ComboComponent combo = (ComboComponent)component;
+		assertNotNull(combo.subComponentA);
+		assertEquals(combo.subComponentA.type, ComponentType.RADIO);
+		assertNotNull(combo.subComponentB);
+		assertEquals(combo.subComponentB.type, ComponentType.TV);
 	}
 }
