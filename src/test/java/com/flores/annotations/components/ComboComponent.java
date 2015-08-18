3e070b06fc689873faf825aa4cd5baebc450769f
Jason <floresj4@gmail.com>
2015-08-18 00:21:43 -0400

    Adding sub components

--- src/test/java/com/flores/annotations/components/ComboComponent.java
+++ src/test/java/com/flores/annotations/components/ComboComponent.java
@@ -1,9 +1,17 @@
 package com.flores.annotations.components;
 
+import com.flores.annotations.Component;
 import com.flores.annotations.ComponentImpl;
 import com.flores.annotations.ComponentType;
 
 public class ComboComponent extends ComponentImpl {
+	
+	@Component(type = ComponentType.RADIO)
+	public ComponentImpl subComponentA;
+
+	@Component(type = ComponentType.TV)
+	public ComponentImpl subComponentB;
+	
 	public ComboComponent() {
 		super(ComboComponent.class, ComponentType.COMBO);
 	}
